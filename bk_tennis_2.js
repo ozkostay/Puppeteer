6d3df4ea5c5fb0d010f918dbba46316f7862b351
ozkostay <ozkostay@mail.ru>
2024-06-06 20:35:46 +0500

    tennis add Labels1

--- bk_tennis_2.js
+++ bk_tennis_2.js
@@ -25,28 +25,58 @@ const app = async () => {
     const retData = [];
     els.forEach((el) => {
       const turnamentId = el.id;
-      const lineRows = [];
+      
+      // ========== Labels
+      const labels = [];
+      const labelTableSource = Array.from(
+        el.querySelectorAll(".coupone-labels")
+      );
 
+      labelTableSource.forEach((i) => {
+        console.log('333', i);
+        const ths = i.querySelectorAll("th");
+        ths.forEach((th) => {
+          // console.log('======= 444' , th.textContent.trim().slice(' ')[0]);
+          console.log('======= 444=' + th.textContent.trim().split(' ')[0] + '=');
+          labels.push(th.textContent.trim().split(' ')[0].replace(/\n/g, ''));
+        })
+
+      })
+      // let LabelThs = [];
+      // console.log('234', labelTableSource[0],'654');
+      // const labelSellsSource = labelTableSource[0].querySelectorAll("th")
+      // console.log('777', labelSellsSource.length,'888');
+      //LabelThs = labelTableSource[0].querySelectorAll("th");
+      // LabelThs.forEach((i, idx) => {
+      //   console.log('123', idx, '===', i);
+      // })
+      
+      // labels.push(labelThs.length);
+      
       // find line_rows
+      const lineRows = [];
       const rows = Array.from(el.querySelectorAll("table.coupon-row-item"));
       const rowsInTurnament = rows.length;
 
       rows.forEach((row, rowIndex) => {
-        // find players in row
+        
+        
+        // labelSource.forEach((i) => labels.push(i.innerText));
+        
+        // =================== find players in row
         const nameSpans = Array.from(
           row.querySelectorAll("span[data-member-link]")
         );
         const players = [];
         nameSpans.forEach((i) => players.push(i.innerText));
 
-        // find data-market-type
+        // ===================  find data-market-type
         const kefsAll = Array.from(row.querySelectorAll("[data-market-type]"));
         const kefsAllTemp = [];
-        //kefsAllTemp.push(kefsAll.length);
         kefsAll.forEach((i) => kefsAllTemp.push(`${i.innerText}`));
 
         if (players.length > 0) {
-          lineRows.push({ rowIndex, players, kefsAllTemp });
+          lineRows.push({ rowIndex, labels, players, kefsAllTemp });
         }
       });
 
@@ -59,7 +89,7 @@ const app = async () => {
   console.log("retrun", data[0]);
   console.log("retrun", JSON.stringify(data[0]));
   console.log("retrun LENGTH", data.length);
-  await browser.close();
+  // await browser.close();
 
   console.log(999);
 }; //end =======
