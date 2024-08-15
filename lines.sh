64619d38e79b7769c586b47fa83e265c96070d65
ozkostay <ozkostay@mail.ru>
2024-08-15 14:04:30 +0500

    all work fine!!!

--- lines.sh
+++ lines.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+echo Начало ===  `date` >> /mnt/arch/linestat_prod/Puppeteer/log-lines.txt
+
+cd /mnt/arch/linestat_prod/Puppeteer/
+
+
+
 
 node bk_tennis_2.js
 node bk_football.js
+
+
+
+
+
+echo Конец ===  `date` >> /mnt/arch/linestat_prod/Puppeteer/log-lines.txt
+
+echo ========================================================================================
+
