4853a8cd452b7a9621e8813370c685c9baf637dc
Konstantin <ozkostay@mail.ru>
2024-08-16 15:07:37 +0500

    results2

--- bk_tennis_results.js
+++ bk_tennis_results.js
@@ -1,9 +1,18 @@
 import puppeteer from "puppeteer";
 import fetch from "node-fetch";
+import { config } from "dotenv";
+config();
 
 const bd = [];
 //==========
 const app = async () => {
+  console.log("process.env.HEADLESS", process.env.HEADLESS);
+  console.log("process.env.SPORT_URL", process.env.SPORT_URL);
+  console.log("process.env.SPORT_PORT", process.env.SPORT_PORT);
+
+  const url_del = `${process.env.SPORT_URL}:${process.env.SPORT_PORT}/tennis/results`;
+  console.log(url_del);
+
   const browser = await puppeteer.launch({
     headless: false, // TRUE - не показывать браузер
   });
@@ -48,10 +57,11 @@ const app = async () => {
     timeWhen.forEach((div) => {
       console.log("=====", div.innerText);
       // if (div.innerText.trim().toLowerCase() === "последние 3 дня") {
-        if (div.innerText.trim().toLowerCase() === "последние 7 дней") {
+      if (div.innerText.trim().toLowerCase() === "последние 7 дней") {
         div.click();
       }
     });
+    return "последние 7 дней";
   });
   console.log(333);
 
@@ -61,16 +71,18 @@ const app = async () => {
     page.keyboard.press("PageDown");
   }
 
-  console.log(333-1);
+  console.log(3331);
 
-  const rowsResults = await page.$$eval("div.result-event", (els) => {
+  const rowsResults = await page.$$eval("div.result-event", async (els) => {
     const gameRowsDOM = Array.from(els);
     const gameRows = [];
     gameRowsDOM.forEach((game) => {
-      console.log('+++', game.querySelector("td.value"));
-      console.log('---', game.querySelector("td.value")?.innerText);
+      console.log("+++", game.querySelector("td.value"));
+      console.log("---", game.querySelector("td.value")?.innerText);
       const newObj = {
-        players: game.querySelector("td.event-name-container").innerText?.trim(),
+        players: game
+          .querySelector("td.event-name-container")
+          .innerText?.trim(),
         result: game.querySelector("td.value")?.innerText.trim(),
         dataResult: game.querySelector("td.date")?.innerText.trim(),
       };
@@ -88,29 +100,37 @@ const app = async () => {
         gameRows.push(newObj);
       }
     });
+
     console.log("ALL", gameRows); // Есть массив с результатами
-
-    // Отправляем на backend
-    const sendOnBackend = async (resultLines) => {
-      console.log("Длина массива", resultLines.length);
-      const options = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          // Authorization: "Bearer " + localStorage.getItem("token"),
-        },
-        body: JSON.stringify(resultLines),
-      };
-      try {
-        const res = await fetch(`http://localhost:3001/tennis/results`, options);
-        console.log("res", await res.json());
-      } catch (e) {
-        console.log("ERROR UPLOAD", e);
-      }
-    };
-
-    sendOnBackend(gameRows);
+    return gameRows;
   });
+
+  console.log("", rowsResults);
+
+  await browser.close(); //========================================================== = = = =
+
+  // Отправляем на backend
+  const sendOnBackend = async (resultLines) => {
+    console.log("Длина массива", resultLines.length);
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+      body: JSON.stringify(resultLines),
+    };
+    try {
+      const url = `${process.env.SPORT_URL}:${process.env.SPORT_PORT}/tennis/results`;
+      console.log('URL', url);
+      const res = await fetch(url, options);
+      console.log("res", await res.json());
+    } catch (e) {
+      console.log("ERROR UPLOAD", e);
+    }
+  };
+
+  sendOnBackend(rowsResults);
 
   console.log(444);
 
