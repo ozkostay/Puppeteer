79c6f52d7f58d38e4b393565cdd440e53556359b
Konstantin <ozkostay@mail.ru>
2024-08-29 18:10:55 +0500

    lines.sh

--- lines.sh
+++ lines.sh
@@ -8,12 +8,13 @@ cd /mnt/arch/linestat_prod/Puppeteer/
 
 node bk_tennis_2.js
 node bk_football.js
+node bk_hockey.js
 
 
 
 
 
-echo Конец ===  `date` >> /mnt/arch/linestat_prod/Puppeteer/log-lines.txt
+echo Конец ===  `date` >> /mnt/arch/linestat_prod/Puppeteer/log-lines.log
 
 echo ========================================================================================
 
