a97c3096eca85374ba9663bc1bd4f70e2114b0ff
Konstantin <ozkostay@mail.ru>
2024-09-13 20:51:37 +0500

    lines.sh add basket

--- lines.sh
+++ lines.sh
@@ -6,15 +6,20 @@ cd /mnt/arch/linestat_prod/Puppeteer/
 
 
 
+echo    Старт ТЕННИС ===  `date` >> /mnt/arch/linestat_prod/Puppeteer/log-lines.log
 node bk_tennis_2.js
+echo    Страт ФУТБОЛ ===  `date` >> /mnt/arch/linestat_prod/Puppeteer/log-lines.log
 node bk_football.js
+echo    Страт ХОККЕЙ ===  `date` >> /mnt/arch/linestat_prod/Puppeteer/log-lines.log
 node bk_hockey.js
+echo    Страт БАСКЕТБОЛ ===  `date` >> /mnt/arch/linestat_prod/Puppeteer/log-lines.log
+node bk_baskenball.js
+echo КонецЛ ===  `date` >> /mnt/arch/linestat_prod/Puppeteer/log-lines.log
 
 
 
 
 
-echo Конец ===  `date` >> /mnt/arch/linestat_prod/Puppeteer/log-lines.log
 
 echo ========================================================================================
 
