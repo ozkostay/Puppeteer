97b51d550a0aa80f914bf9f0f1ba2122d0a8b96a
ozkostay <ozkostay@mail.ru>
2024-10-13 11:24:59 +0300

    131024

--- lines.sh
+++ lines.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-
 script_dir="/var/Puppeteer"
 log_file="$script_dir/log-lines.log"
 
-
 echo = >> $log_file
+
+if [[ "$HOSTNAME" == "konst-ub22" ]]; then
+  echo "Host work PROD"
+  node_bin=/home/konst/.nvm/versions/node/v20.14.0/bin/node
+elif [[ "$HOSTNAME" == "linestat-mate" ]]; then
+  echo "Host home PROD"
+  node_bin=/home/konst/.nvm/versions/node/v20.17.0/bin/node
+else
+  echo "Host NOT PROD"
+  node_bin=/home/konst/.nvm/versions/node/v20.15.1/bin/node
+fi
 
 
 echo Начало ===  `date` >> $log_file
@@ -13,16 +22,16 @@ echo Начало ===  `date` >> $log_file
 cd $script_dir
 
 echo    Старт ТЕННИС    ===  `date` >> $log_file
-node bk_tennis_2.js
+$node_bin bk_tennis_2.js
 
 echo    Страт ФУТБОЛ    ===  `date` >> $log_file
-node bk_football.js
+$node_bin bk_football.js
 
 echo    Страт БАСКЕТБОЛ ===  `date` >> $log_file
-node bk_basketball.js
+$node_bin bk_basketball.js
 
 echo    Страт ХОККЕЙ    ===  `date` >> $log_file
-node bk_hockey.js
+$node_bin bk_hockey.js
 
 echo Конец ===  `date` >> $log_file
 
